d642d989bfe557ca7ba6b560ab5bba683fb20782
Biswaranjan Rout <biswaranjan.rout@xceedance.com>
2018-06-09 01:23:05 +0530

    Completed express api

--- config/key.js
+++ config/key.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://routbisu:Devconnect%402018@ds016128.mlab.com:16128/devconnect'
+  mongoURI: 'mongodb://routbisu:Devconnect%402018@ds016128.mlab.com:16128/devconnect',
+  secretOrKey: 'lifeafteryou'
 }
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,14 +9,24 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
-const db = require('./config/key').mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db).then(() => console.log('Mongo DB connected!'))
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.json({ message: 'Welcome to DevConnect API' }));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
@@ -23,5 +35,4 @@ app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
-
-app.listen(port, () => { console.log(`Server running on port ${port}`); });
+app.listen(port, () => console.log(`Server running on port ${port}`));
