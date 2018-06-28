0dc303158759a92885d7625a674f065832bbdb8f
Biswaranjan Rout <biswaranjan.rout@xceedance.com>
2018-06-28 14:26:07 +0530

    Setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,10 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import store from './store';
+
+
+
 class App extends Component {
   render() {
     return (
