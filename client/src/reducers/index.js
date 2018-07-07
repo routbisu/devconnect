2f4ff9b76c73afecd8bf475504106982e0edf8d5
Biswaranjan Rout <biswaranjan.rout@xceedance.com>
2018-07-07 18:40:31 +0530

    added profile reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,7 @@
 import authReducer from './authReducer';
 import { combineReducers } from 'redux';
 import errorReducer from './errorReducer';
+import profileReducer from './errorReducer';
 
 export default combineReducers({
   auth: authReducer,
